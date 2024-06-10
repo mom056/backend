560694c1ef897b8ecbaf2da37c23145169d1a057
mom056 <99486386+mom056@users.noreply.github.com>
2024-06-10 19:07:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,24 @@ const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 
-
-mongoose.connect(process.env.DB_KEY)
-   
-
+mongoose.connect(process.env.DB_KEY, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to the database');
+}).catch((err) => {
+    console.error('Database connection error:', err);
+});
 
 const app = express();
 
-app.use(require('cors')());
+app.use(cors());
 app.use(express.json());
 
-
-
-require('./routes')(app)
+// Include routes
+require('./routes')(app);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
-app.listen(process.env.PORT||4000, ()=> {
-    console.log('server is running')
-}) 
+app.listen(process.env.PORT || 4000, () => {
+    console.log('Server is running on port', process.env.PORT || 4000);
+});
